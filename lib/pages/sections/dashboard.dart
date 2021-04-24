85957cc37cc9b37e925b08b663cfc0361daf8e86
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2021-04-24 18:49:17 +0000

    Updatin dashboard

--- lib/pages/sections/dashboard.dart
+++ lib/pages/sections/dashboard.dart
@@ -4,14 +4,38 @@ class Dashboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            padding: EdgeInsets.all(10),
-            child: Image(image: AssetImage('assets/images/banner.jpg')),
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Container(
+                height: 220,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage('assets/images/banner.jpg'),
+                      fit: BoxFit.cover),
+                  borderRadius: BorderRadius.circular(12),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.grey[600],
+                        blurRadius: 8,
+                        offset: Offset(0, 6))
+                  ],
+                )),
+            SizedBox(height: ,),
+            buildFirstCard(),
+          ],
+        ),
       ),
     );
   }
+
+  Widget buildFirstCard() => Card(
+        shadowColor: Colors.grey[600],
+        elevation: 6,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+        child: Container(
+          padding: EdgeInsets.all(10),
+        ),
+      );
 }
